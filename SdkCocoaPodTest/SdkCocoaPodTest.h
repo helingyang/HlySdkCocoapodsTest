0b0108b48f1a45e536bc8bfd32f1327f0112a5c7
HLY <hly091516@163.com>
2020-05-14 18:02:45 +0800

    上传试试

---
+++ SdkCocoaPodTest/SdkCocoaPodTest.h
@@ -0,0 +1,13 @@
+//
+//  SdkCocoaPodTest.h
+//  SdkCocoaPodTest
+//
+//  Created by 天才哈士奇 on 2020/5/14.
+//  Copyright © 2020 天才哈士奇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SdkCocoaPodTest : NSObject
++(void)testHlyCocoapods;
+@end
---
+++ SdkCocoaPodTest/SdkCocoaPodTest.m
@@ -0,0 +1,17 @@
+//
+//  SdkCocoaPodTest.m
+//  SdkCocoaPodTest
+//
+//  Created by 天才哈士奇 on 2020/5/14.
+//  Copyright © 2020 天才哈士奇. All rights reserved.
+//
+
+#import "SdkCocoaPodTest.h"
+
+@implementation SdkCocoaPodTest
++ (void)testHlyCocoapods{
+    
+    NSLog(@"HlyCocoapodsSDK测试");
+    
+}
+@end
